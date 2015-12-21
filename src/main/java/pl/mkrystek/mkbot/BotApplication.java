693293f7f1d7e04aeb8f9c90e1447bd8364413d2
mkrystek <krystek.michal@gmail.com>
2015-12-21 20:41:30 +0100

    Actually run the application

--- src/main/java/pl/mkrystek/mkbot/BotApplication.java
+++ src/main/java/pl/mkrystek/mkbot/BotApplication.java
@@ -2,6 +2,7 @@ package pl.mkrystek.mkbot;
 
 import static java.util.concurrent.Executors.newSingleThreadScheduledExecutor;
 
+import java.util.concurrent.CountDownLatch;
 import pl.mkrystek.mkbot.task.TaskExecutionEngine;
 import pl.mkrystek.mkbot.task.TaskProvider;
 import pl.mkrystek.mkbot.window.SkypeWindow;
@@ -11,21 +12,24 @@ public class BotApplication {
     private final TaskExecutionEngine taskExecutionEngine;
     private final TaskProvider taskProvider;
     private final SkypeWindow skypeWindow;
+    private final CountDownLatch latch;
 
     public BotApplication() throws Exception {
         this.skypeWindow = new SkypeWindow();
         this.taskExecutionEngine = new TaskExecutionEngine(newSingleThreadScheduledExecutor(), skypeWindow);
         this.taskProvider = new TaskProvider();
+        this.latch = new CountDownLatch(1);
     }
 
     public void init() {
         taskExecutionEngine.init(taskProvider.getReplyTasks(), taskProvider.getScheduledTasks());
     }
 
-    public void startApplication() {
+    public void startApplication() throws InterruptedException {
         if (skypeWindow.bringToForeground()) {
             taskExecutionEngine.start();
         }
+        latch.await();
     }
 
     public void shutdown() {
