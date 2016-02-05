cc5eb4320560e1a4cf25575d342dc59b438b7b84
mkrystek <krystek.michal@gmail.com>
2016-02-05 09:48:08 +0100

    Let Spring handle application properties loading

--- src/main/java/pl/mkrystek/mkbot/BotProperties.java
+++ src/main/java/pl/mkrystek/mkbot/BotProperties.java
@@ -1,44 +1,32 @@
 package pl.mkrystek.mkbot;
 
-import java.io.IOException;
-import java.io.InputStream;
-import java.util.Properties;
-
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
 @Component
 public class BotProperties {
 
-    private static final Logger LOGGER = LoggerFactory.getLogger(BotProperties.class);
-    private static final String PROPERTIES_FILE_NAME = "config.properties";
-
-    private String skypeDbPath;
+    @Value("${chat_name}")
     private String chatName;
+
+    @Value("${polling_frequency}")
     private Long pollingFrequency;
+
+    @Value("${skype_username}")
     private String skypeUsername;
+
+    @Value("${reply_tasks_to_load}")
     private String replyTasksToLoad;
+
+    @Value("${scheduled_tasks_to_load}")
     private String scheduledTasksToLoad;
 
-    public BotProperties() {
-        try {
-            Properties props = new Properties();
-            InputStream propertiesFile = BotProperties.class.getClassLoader().getResourceAsStream(PROPERTIES_FILE_NAME);
-            props.load(propertiesFile);
-            skypeUsername = props.getProperty("skype_username");
-            skypeDbPath = String.format("%s\\Skype\\%s\\main.db", System.getenv("APPDATA"), skypeUsername);
-            chatName = props.getProperty("chat_name");
-            pollingFrequency = Long.parseLong(props.getProperty("polling_frequency"));
-            replyTasksToLoad = props.getProperty("reply_tasks_to_load");
-            scheduledTasksToLoad = props.getProperty("scheduled_tasks_to_load");
-            propertiesFile.close();
-        } catch (IOException e) {
-            LOGGER.error("Error occurred while reading properties file: ", e);
-        }
-    }
+    private String skypeDbPath;
 
     public String getSkypeDbPath() {
+        if (skypeDbPath == null) {
+            skypeDbPath = String.format("%s\\Skype\\%s\\main.db", System.getenv("APPDATA"), skypeUsername);
+        }
         return skypeDbPath;
     }
 
